48db6c6ddc75173fac4c1f6435dadd4b7063ff9f
lemontech119 <lemontech119@gmail.com>
2019-07-14 00:05:32 +0900

    데이터베이스 설정 에러 방지

--- ttokproject/src/main/java/com/example/ttokproject/TtokprojectApplication.java
+++ ttokproject/src/main/java/com/example/ttokproject/TtokprojectApplication.java
@@ -1,12 +1,17 @@
 package com.example.ttokproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
+
 public class TtokprojectApplication {
 
     @GetMapping("/test")
