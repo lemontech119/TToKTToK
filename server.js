a1faa475cfc4256d769100f50029c4920d15fd88
lemontech119 <lemontech119@gmail.com>
2019-09-30 16:57:02 +0900

    routes 테스트 코드

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express=require("express");
 const path=require("path");
 const app=express(); 
+const morgan = require('morgan');
 const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('dev'));
+
+const member = require("./router/member");
+
+app.use("/member", member);
 
 
 app.get('/', (req, res) => {
@@ -10,4 +20,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Server listening ...' + port);
-})
+});
