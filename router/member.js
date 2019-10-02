da9d18c6e7adcc451aeb49a4ecf5be9a3c1789ee
lemontech119 <lemontech119@gmail.com>
2019-10-02 19:48:15 +0900

    post test

--- router/member.js
+++ router/member.js
@@ -15,11 +15,41 @@ router.get("/login", (req, res) =>{
     res.render("member/login.ejs");
 })
 
+router.post("/login", (req, res) =>{
+    let user_id = req.body.userid;
+    let password = req.body.password;
+    
+
+    console.log("user_id " + user_id);
+    console.log("password " + password);
+    
+
+    res.send("post test");
+})
+
 
 
 // 회원가입 창
 router.get("/join", (req, res) =>{
     res.render("member/join.ejs");
+})
+
+router.post("/join", (req, res) =>{
+    let user_id = req.body.userid;
+    let password = req.body.password;
+    let name = req.body.name;
+    let sql = "insert into ttokttok.member(member_id, password, name) values (?, ?, ?)";
+
+    connection.query(sql, [user_id, password, name], function(err, rows){
+        if(!err){
+            console.log("The solution is ", rows);
+            res.send("성공!");
+        }else{
+            res.send("실패 ㅠㅠ");
+        }
+    })
+
+    
 })
 
 router.get("/dbtest", (req, res) => {
