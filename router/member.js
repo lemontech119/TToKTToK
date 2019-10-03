d13bfc976d81c4f1819aca94303b7b490c0eb080
lemontech119 <lemontech119@gmail.com>
2019-10-03 16:21:33 +0900

    회원가입, 로그인 테스트

--- router/member.js
+++ router/member.js
@@ -4,11 +4,15 @@ const mysql = require('mysql');
 const conn = require("../dbconnection.js");
 const connection = mysql.createConnection(conn);
 
-router.get("/", (req, res) => {
-    
-    res.render("test.ejs");
+router.get('/logout', (req, res) => {
+    console.log('로그아웃시도1');
+    console.log(req.session.user);
+    req.session.destroy(function () {
+        req.session;
+    });
+    console.log('로그아웃시도2');
+    res.redirect('/');
 })
-
 
 // 로그인 창
 router.get("/login", (req, res) =>{
@@ -18,16 +22,35 @@ router.get("/login", (req, res) =>{
 router.post("/login", (req, res) =>{
     let user_id = req.body.userid;
     let password = req.body.password;
-    
-
-    console.log("user_id " + user_id);
-    console.log("password " + password);
-    
-
-    res.send("post test");
+    let sql = "select * from ttokttok.member where member_id = ?";
+    connection.query(sql, [user_id], function(err, rows){
+        if(!err){
+            console.log(rows);
+            if(rows.length == 0){
+                console.log("ID 잘못 적은 케이스");
+                res.render("member/login.ejs");
+                return;
+            }else if(password != rows[0].password){
+                console.log("ID는 맞았지만 비밀번호가 다름");
+                res.render("member/login.ejs");
+                return;
+            }else{
+                console.log("성공이당");
+                req.session.user = user_id;
+                req.session.save(function(){
+                    res.redirect("/");
+                })
+                return;
+            }
+            
+            
+            console.log("이거 나오면 안되는데...");
+        }else{
+            console.log(err);
+            res.send(err);
+        }
+    })    
 })
-
-
 
 // 회원가입 창
 router.get("/join", (req, res) =>{
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path=require("path");
 const app=express(); 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieparser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
@@ -13,13 +16,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan('dev'));
 
+app.use(cookieparser());
+app.use(session({
+    secret: '1A@W#E$E',
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore()
+}));
+
+
 const member = require("./router/member");
 
 app.use("/member", member);
 
 
 app.get('/', (req, res) => {
-    res.send("test test");
+    res.render("index.ejs");
 })
 
 app.listen(port, () => {
